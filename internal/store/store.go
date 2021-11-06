65ce1e872c3c8d9e8a09f679d092de8ab59b821d
Parham Alvani <1995parham@tuta.io>
2021-11-06 23:04:20 +0330

    feat: add distance store

---
+++ internal/store/store.go
@@ -0,0 +1,22 @@
+package store
+
+import "sync"
+
+type Distance struct {
+	value int64
+	lock  sync.RWMutex
+}
+
+func (d *Distance) Get() int64 {
+	d.lock.RLock()
+	defer d.lock.Unlock()
+
+	return d.value
+}
+
+func (d *Distance) Set(value int64) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+
+	d.value = value
+}
