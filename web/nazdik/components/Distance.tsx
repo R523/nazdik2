e8f1635b5c027afc7c35da8e835a1d6a50470a11
Parham Alvani <1995parham@tuta.io>
2021-11-06 21:45:24 +0330

    feat: add distance component

---
+++ web/nazdik/components/Distance.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import Slider from "@mui/material/Slider";
+
+export default function Distance() {
+  let [distance, setDistance] = useState(200);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      fetch("/app")
+        .then((resp) => {
+          if (resp.ok) {
+            return resp.json();
+          }
+          throw new Error(`request failed with ${resp.statusText}`);
+        })
+        .then((data) => setDistance(distance - 1))
+        .catch((err) => console.log(err));
+    }, 1000);
+
+    return () => clearInterval(timer);
+  });
+
+  return <Slider disabled value={distance} step={10} marks min={0} max={200} />;
+}
