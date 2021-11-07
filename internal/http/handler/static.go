ed62fc1043e5351f45c0443dac0930eea81f7d55
Parham Alvani <1995parham@tuta.io>
2021-11-07 08:09:49 +0330

    feat: add static file handler

--- internal/http/handler/static.go
+++ internal/http/handler/static.go
@@ -1,21 +1,23 @@
 package handler
 
 import (
-	"time"
+	"io/fs"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
 )
 
 const MaxAge = 3600
 
-func Static(app *fiber.App) {
-	app.Static("/", "../../web/nazdik/out", fiber.Static{
-		Compress:      true,
-		ByteRange:     false,
-		Browse:        false,
-		CacheDuration: time.Hour,
-		MaxAge:        MaxAge,
-		Index:         "index.html",
-		Next:          nil,
-	})
+func Static(app *fiber.App, content fs.FS) {
+	app.Use("/", filesystem.New(filesystem.Config{
+		Root:         http.FS(content),
+		PathPrefix:   "",
+		NotFoundFile: "404.html",
+		Browse:       false,
+		MaxAge:       MaxAge,
+		Index:        "index.html",
+		Next:         nil,
+	}))
 }
